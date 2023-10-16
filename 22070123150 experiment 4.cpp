b8f34b416b990cd694dee8fd3a88e91f4f0a7de4
Arjun Gohad <74441883+Arjun378@users.noreply.github.com>
2023-10-16 15:32:06 +0530

    Add files via upload

---
+++ 22070123150 experiment 4.cpp
@@ -0,0 +1,17 @@
+/*22070123150 Experiment 4*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 67; 
+    int bit_to_be_set;
+    cout<<"Enter the bit set: ";
+    cin>>bit_to_be_set;
+    int k = i|(1<<bit_to_be_set);
+    int bit_to_be_reset;
+    cout<<"Enter th5e bit reset: ";
+    cin>>bit_to_be_reset;
+    int j = i&(~(1<<bit_to_be_reset));
+    cout<<k<<endl;
+    cout<<j<<endl;
+}
